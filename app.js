590eecdfe67291465416ae7e17cfcc78a47a6b12
Umer-FarooqAhmed <123248058+Umer-Farooq-Ahmed@users.noreply.github.com>
2023-02-05 23:27:21 +0500

    app.js.file Js Assignment no4
    app.js.file Js Assignment no4

---
+++ app.js
@@ -0,0 +1,306 @@
+// ****** Chapter no9to11 
+//  Question no1
+// var cityName =prompt('enter your city name',)
+// if(cityName === "karachi"){
+//     alert("welcome to city of lights")
+// }
+
+//  Question no2
+// var cityName =prompt('enter your gender',);
+// if(cityName === "Male"){
+//     alert("good mornimg sir;")
+// }
+// else{
+//    (cityName === "Female")
+//    alert("good morning madam")
+// }
+
+//  Question no3
+// var signals =prompt('enter signal ligth color',);
+// if(signals === "red"){
+//     alert("must stop")
+// }
+// else if (signals === "yellow"){
+//     alert("ready to move")
+// }
+// else{
+//   (signals === "green")
+//    alert("move now")
+// }
+
+//  Question no4
+// var car =prompt('fuel kitna ha',);
+// if(car === "0.25"){
+//     alert("ok good")
+// }
+// else if (car === "0.10"){
+//     alert("fuel pora kro")
+// }
+// else{
+//   (car === "0.30")
+//    alert("fuel ziada dal dia ha chalo koi bt nh")
+// }
+
+// //  Question no5(A)
+// var a=(4)
+// if (++a === 5){
+//    console.log(true)
+// }
+
+//  Question no5(b)
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+//  Question no5(c)
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+//  Question no5(d)
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+//  Question no5(f)
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+// //  Question no6(a)
+// var percentage =prompt('enter your percentage',);
+// if(percentage === "80"){
+//     alert("A-one")
+// }
+// else if (percentage === "70"){
+//     alert("A-grade")
+// }
+// else{
+//   (percentage === "60")
+//    alert("B-grade")
+// }
+
+// // //  Question no6(b)
+// document.write("<h1>"+"Marks sheet"+"</h1>")
+// var marksSheet =prompt("MarksSheet","Total mark","Mark obtained","percentage","grade","remarks")
+
+//  //  Question no7(a)
+//  var secretNumber =prompt("one to ten ma say koi aik number bate jo sahi jawab ho")
+//  if(secretNumber === "7"){
+//     alert("correct answer")
+//  }
+
+//   //  Question no7(b)
+//   var secretNumber =prompt("one to ten ma say koi aik number bate jo sahi jawab ho")
+//   if(secretNumber === "7"){
+//      alert("correct answer")
+//   }
+//   else{
+//     alert("wrong answer")
+//   }
+
+//   //  Question no8
+//   var secretNumber =prompt("one to ten ma say koi aik number bate jo sahi jawab ho")
+//   if(secretNumber === "7"){
+//      alert("correct answer "+7*3)
+//   }
+//   else{
+//     alert("wrong answer")
+//   }
+
+// //   Question no9
+//   var num1 =prompt("koi 1to9 ma say koi number likha ")
+//   if(num1 === "1","3","5","7","9"){
+//      alert("even number")
+//   }
+//   else if (num1 === "2","4","6","8" ){
+//     alert("odd number")
+//   }
+//   else{
+//     alert("wrong answer")
+//   }
+
+// //  Question no10
+// var temperature=prompt("temperature")
+// if(temperature === "40"){
+//     alert("“It is too hot outside.”")
+// }
+// else if (temperature === "30"){
+//     alert("“The Weather today is Normal.”")
+// }
+// else if(temperature === "20"){
+//     alert("“Today’s Weather is cool.”")
+// }
+// else if(temperature === "10"){
+//     alert("“OMG! Today’s weather is so Cool.”")
+// }
+// else{
+//     alert("not define")
+// }
+
+// //  Question no11(a)
+// var calculator1 =+prompt("first Number")
+// var calculator2 =+prompt("second number")
+
+// alert(calculator1 + calculator2)
+
+//  Question no11(b)
+// var calculator1 =+prompt("first Number")
+// var calculator2 =+prompt("second number")
+
+// alert(calculator1 - calculator2)
+
+//  Question no11(c)
+// var calculator1 =+prompt("first Number")
+// var calculator2 =+prompt("second number")
+
+// alert(calculator1 * calculator2)
+
+//  Question no11(d)
+// var calculator1 =+prompt("first Number")
+// var calculator2 =+prompt("second number")
+
+// alert(calculator1 / calculator2)
+
+// //  Question no11(e)
+// var calculator1 =+prompt("first Number")
+// var calculator2 =+prompt("second number")
+
+// alert(calculator1 % calculator2)
+
+// *****Chapter no12to13
+// //  Chapterno1(a)
+// var a = prompt("ASCII");
+//  if (a === "A") {
+//  alert("A=65");
+//  }
+
+// //  Chapterno1(b)
+// var a = prompt("ASCII");
+//  if (a === "Z=90") {
+//  alert(" z=122");
+//  }
+
+// //  Chapterno2
+// var a =+prompt("A")
+// var b =+prompt("B")
+// if (a === b){
+//     alert("A === B")
+// }
+// else if (b === a){
+//         alert("B === A")
+//     }
+
+// //  Chapterno3
+// var a = prompt("0to9 only one digit");
+//  if (a === "0") {
+//  alert("zero");
+//  }
+//  else if (a === "1"){
+//     alert("negative number");
+//  }
+//  else{
+//     alert("positive number")
+//  }
+
+// //  Chapterno4
+// var a = prompt("Weight");
+// var a = prompt("Time");
+// var a = prompt("Age");
+// var a = prompt("Gender");
+
+// if (weight > 300) {
+//     alert("Apply now");
+//     }
+// else   if (time < 6) {
+//         alert("Apply now");
+//   }
+//   else if (Age> 17 ) {
+//             alert("Apply now");
+//  }
+//  else if ( Gender === "male") {
+//                 alert("Apply now");
+//      }
+//     else {
+//     alert("Sorry");
+//     }
+
+// // Question no5
+// var pasword=prompt("type password")
+// if (password === 1234){
+//     alert("correct")
+// }
+// else{
+//     alert("plz correct password")
+// }
+
+// // Question no6
+// var hour =prompt ("time batao");
+// if (hour < 18) {
+// alert("Good day");
+// }
+// else{
+// alert("Good evening");
+// }
+
+// // Question no7
+// var hour = prompt("time batao");
+// if (hour === "12") {
+//     alert("12am");
+// }
+// else if (hour === "13") {
+//     alert("1pm");
+// }
+// else if (hour === "14") {
+//     alert("2");
+// }
+// else if (hour === "15") {
+//     alert("3pm");
+// }
+// else if (hour === "16") {
+//     alert("4pm");
+// }
+// else if (hour === "17") {
+//     alert("5pm");
+// }
+// else if (hour === "18"){
+//     alert("6pm");
+// }
+// else if (hour === "19") {
+//     alert("7pm");
+// }
+// else if (hour === "20") {
+//     alert("8pm");
+// }
+// else if (hour === "21") {
+//     alert("9pm");
+// }
+// else if (hour === "22") {
+//     alert("10pm");
+// }
+// else if (hour === "23") {
+//     alert("11pm");
+// }
+// else {
+//     alert("12pm");
+// }
